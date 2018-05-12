33c87667e84b0a481f13aa0b1da49e800abd3a3c
danielwikstrom <danielwikstrom22@hotmail.com>
2018-05-12 02:52:22 +0200

    Add starting project

---
+++ Source/HexRTS/HexRTSGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HexRTSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEXRTS_API AHexRTSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
